0cbe5d282f4981f688f7aa80712db2e6f5fa00c3
lllcloudstudio <zznailmail@gmail.com>
2026-06-18 21:56:37 -0400

    SEE at html-rpg feature_frequency_table_files ???
    ???

--- script.r
+++ script.r
@@ -1,11 +1,11 @@
 library(e1071)
-table=read.table('/cloud/project/kolachalama_data.csv',sep=',',header=TRUE)
+table=read.table('C:\\Users\\aflac\\Documents\\GitHub\\html-Rpg\\kolachalama_data.csv',sep=',',header=TRUE)
 attach(table)
 Category <- sample(c(0, 1), 50, replace = TRUE) # random vector not attached to table, factor issue
 table[["Category"]] <- Category # add category with random labels
 table=table[,c("ph","Hb","LDL","HDL","Category")]
 
-
+### Table for model 1
 table.1=table[,c("ph","Hb","Category")] # 
 X <- subset(table.1, select = -Category)
 y <- Category
@@ -14,8 +14,9 @@ model <- svm(X, y) # same as print(model) or summary(model)
 pred<-predict(model,X)
 pred_table=table(pred, y) #
 #cat(capture.output(pred_table),"\n,", file="/cloud/project/out/out_2.file",sep=",", header=FALSE,append =TRUE,labels=NULL)
-write.table(as.data.frame(pred_table), "/cloud/project/out/out_2.file", row.names = FALSE,col.names=FALSE,append=TRUE,sep=',')
+write.table(as.data.frame(pred_table), "C:\\Users\\aflac\\Documents\\GitHub\\html-Rpg\\out_2.file", row.names = FALSE,col.names=FALSE,append=TRUE,sep=',')
 
+# Table for model 2
 table.2=table[,c("LDL","HDL","Category")]
 X <- subset(table.2, select = -Category)
 y <- Category
@@ -23,27 +24,25 @@ model <- svm(X, y) # same as print(model) or summary(model)
 # unavailable: plot(model,X)
 pred<-predict(model,X)
 pred_table=table(pred, y) #
-write.table(as.data.frame(pred_table), "/cloud/project/out/out_2.file", row.names = FALSE,col.names=FALSE,append=TRUE,sep=',')
+write.table(as.data.frame(pred_table), "C:\\Users\\aflac\\Documents\\GitHub\\html-Rpg\\out_2.file", row.names = FALSE,col.names=FALSE,append=TRUE,sep=',')
 
 ###########################################
 library(dygraphs)
 library(widgetframe)
 library(htmlwidgets)
-
+### Example
 # Create a time-series widget with dygraph
-ts_widget <- dygraph(nhtemp, main = "New Haven Temperatures")
-
+#ts_widget <- dygraph(nhtemp, main = "New Haven Temperatures") # toggle or cursor feature of dygraph
 # Save it specifically formatted for responsive iframes
-saveWidget(frameableWidget(ts_widget), file = "embedded_chart.html", selfcontained = TRUE)
-
+#saveWidget(frameableWidget(ts_widget), file = "embedded_chart.html", selfcontained = TRUE)
 # Once exported, copy embedded_chart.html into your web server directory and call it from your primary HTML source file like this:
 #<iframe src="embedded_chart.html" width="100%" height="400px" style="border:none;"></iframe>
   
 ###########################################
 library(DT)
-library(htmlwidgets)
 
-table=read.table('/cloud/project/out/out_2.file',sep=",",header=FALSE)
+### Create an interactive html table of 2 models
+table=read.table('C:\\Users\\aflac\\Documents\\GitHub\\html-Rpg\\out_2.file',sep=",",header=FALSE)
 # Set parameters for the feature
 data_vec <- table$V1#example: iris$Sepal.Length
 bin_w <- 0.2                     # Choose a meaningful bin width for your data
@@ -64,25 +63,54 @@ freq_table$Cumulative_Freq <- cumsum(freq_table$Freq)
 # View the formatted summary table OR
 print(freq_table)
 #OR
+
+# Create your interactive data table widget
 # 3. Render the  table
 my_interactive_table <-datatable(
   freq_table,
   colnames = c("Value Interval", "Frequency (Count)", "Percentage (%)", "Cumulative Count"),
-  options = list(pageLength = 10, dom = 'ftp', searching = TRUE),
+  options = list(pageLength = 10, dom = 'ftp', searching = TRUE), # or not searching = TRUE
   caption = 'Table 1: Binned Frequency Distribution for Continuous Feature.'
 )
-### 
-hist(table$V1)
-# Base R alternative to hist()
-plot(density(table$V1),main="Density Plot",col="blue",lwd=2)
-polygon(density(table$V1),col="lightblue",border="blue")
-
-# Create your interactive data table widget
-my_interactive_table <- datatable(
-  freq_table,
-  colnames = c("Value Interval", "Frequency (Count)", "Percentage (%)", "Cumulative Count"),
-  options = list(pageLength = 10, dom = 'ftp')
-) # just caption excluded includes search bar
 
 # Export to a standalone HTML file in your current working directory
-saveWidget(my_interactive_table, "/cloud/project/feature_frequency_table.html", selfcontained = TRUE) # ref src
+saveWidget(my_interactive_table, "C:\\Users\\aflac\\Documents\\GitHub\\html-Rpg\\feature_frequency_table.html", selfcontained = TRUE) # ref src
+#<iframe src="C:/Users/aflac/Downloads/feature_frequency_table.html" width="100%" height="400px" style="border:none;"></iframe>
+
+# OR a density graph
+#
+data=table$V1
+hist(table$V1)
+
+# Calculate statistics
+mean_val <- mean(data, na.rm = TRUE)
+median_val <- median(data, na.rm = TRUE)
+q25      <- quantile(data_vec, 0.25, na.rm = TRUE)
+q75      <- quantile(data_vec, 0.75, na.rm = TRUE)
+
+# Create histogram
+hist(data,
+     main = "Histogram with Mean and Median Lines",
+     xlab = "Values",
+     col = "lightblue",
+     border = "white")
+
+# Add mean line (red, dashed)
+abline(v = mean_val, col = "red", lwd = 2, lty = 2)
+# Add median line (blue, solid)
+abline(v = median_val, col = "blue", lwd = 2, lty = 1)
+abline(v=q25,col="green",lwd=2,lty=1) # FIX
+abline(v=q75,col="yellow",lwd=2,lty=2) # FIX
+
+# Add legend
+legend("topright",
+       legend = c(paste0("Mean = ", round(mean_val, 2)), # ADD
+                  paste0("Median = ", round(median_val, 2))), #ADD
+       col = c("red", "blue"),
+       lty = c(2, 1),
+       lwd = 2,
+       bty = "n")
+# Base R alternative to hist()
+# See if it can be made interactive with toggle cursor
+plot(density(table$V1),main="Density Plot",col="blue",lwd=2)
+polygon(density(table$V1),col="lightblue",border="blue")
